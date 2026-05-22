80c74566060167e074bdf79a1125e4095b004869
HerrderIdioten <nicolay.burri@stud.abbts.ch>
2026-05-22 09:01:24 +0200

    Changed to ssl

--- Areas/Identity/Data/EmailService.cs
+++ Areas/Identity/Data/EmailService.cs
@@ -34,6 +34,7 @@ namespace FerienspassWebApp.Areas.Identity.Data
             {
                 using var smtp = new SmtpClient();
 
+                //testsetup
                 await smtp.ConnectAsync(
                     _config["Email:Smtp"],
                     int.Parse(_config["Email:Port"]),
