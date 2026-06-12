4d9eb08cad7a5807c69c0e8914b09b801518459c
HerrderIdioten <nicolay.burri@stud.abbts.ch>
2026-06-12 03:21:00 +0200

    Addedjusted Migrations

---
+++ Migrations/20260612011800_FixPendingModelChanges.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FerienspassWebApp.Migrations
+{
+    /// <inheritdoc />
+    public partial class FixPendingModelChanges : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "DocumentUrl",
+                table: "Events",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DocumentUrl",
+                table: "Events");
+        }
+    }
+}
